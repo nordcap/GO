a07eec21dd9bb698409d87fcf462353e9a56cce3
Alexey Budaev <budaevaa@mail.ru>
2021-03-20 09:29:23 +0700

    commit34

---
+++ Функции, структуры и др/7 Решение задач/1.go
@@ -0,0 +1,14 @@
+//На вход подаются a и b - катеты прямоугольного треугольника. Нужно найти длину гипотенузы
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var a, b float64
+	fmt.Scan(&a, &b)
+	c := math.Sqrt(a*a + b*b)
+	fmt.Println(c)
+}
